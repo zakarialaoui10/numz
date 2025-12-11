08b654e21993d209c33178d06621155b901f96e0
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-12-11 13:03:36 +0100

    add stats

---
+++ src/stats/index.js
@@ -0,0 +1,5 @@
+export * from './percentile/index.js';
+export * from './average/index.js';
+export * from './variability/index.js'
+export * from './rolling/index.js'
+export * from './accum/index.js'
